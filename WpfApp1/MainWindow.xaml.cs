7589b694b1cc3d3e7a31878b5b98cb6f4e84612f
TerraMax <you@example.com>
2018-02-22 20:05:48 +0100

    Implementation of namespace WpfApp1.LocalAuth containing: - LauncherCredentials - User - UnknownUserException

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Xml;
+using WpfApp1.LocalAuth;
 
 namespace WpfApp1
 {
@@ -27,7 +18,186 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            
+        }
+    }
 
+    namespace LocalAuth
+    {
+        public class LauncherCredentials : XmlDocument
+        {
+            enum Apps { Steam, Origin, Uplay, Lol };
+
+            public LauncherCredentials()
+            {
+                XmlDeclaration xmlDeclaration = this.CreateXmlDeclaration("1.0", "UTF-8", null);
+                XmlElement root = this.CreateElement("Users");
+                this.InsertAfter(xmlDeclaration, root);
+            }
+
+            public LauncherCredentials(string fileName)
+            {
+                if (File.Exists(fileName))
+                {
+                    this.Load(fileName);
+                }
+                else
+                {
+                    XmlDeclaration xmlDeclaration = this.CreateXmlDeclaration("1.0", "UTF-8", null);
+                    XmlElement root = this.CreateElement("Users");
+                    this.InsertAfter(xmlDeclaration, root);
+                    this.Save(fileName);
+                }
+            }
+
+            public User[] GetAllUsers()
+            {
+                User[] Users;
+
+                XmlNodeList XmlUsers = this.GetElementsByTagName("User");
+                int UserCount = XmlUsers.Count;
+                Users = new User[UserCount];
+                int Counter = 0;
+                foreach (XmlNode XmlUser in XmlUsers)
+                {
+                    int.TryParse(XmlUser.Attributes.GetNamedItem("App").InnerText, out int UserApp);
+                    Users[Counter] = new User(XmlUser.FirstChild.InnerText, XmlUser.LastChild.InnerText, UserApp);
+                    Counter++;
+                }
+
+                return Users;
+            }
+
+            public User[] GetAllUsers(int app)
+            {
+                User[] Users;
+
+                XmlNodeList XmlUsers = this.GetElementsByTagName("User");
+                int Counter = 0;
+                int UserCounter = 0;
+                foreach (XmlNode XmlUser in XmlUsers)
+                {
+                    if (XmlUser.Attributes.GetNamedItem("App").InnerText == app.ToString())
+                    {
+                        UserCounter++;
+                    }
+                    Counter++;
+                }
+
+                Users = new User[UserCounter];
+                Counter = 0;
+                foreach (XmlNode XmlUser in XmlUsers)
+                {
+                    if (XmlUser.Attributes.GetNamedItem("App").InnerText == app.ToString())
+                    {
+                        int.TryParse(XmlUser.Attributes.GetNamedItem("App").InnerText, out int UserApp);
+                        Users[Counter] = new User(XmlUser.FirstChild.InnerText, XmlUser.LastChild.InnerText, UserApp);
+                        Counter++;
+                    }
+                }
+
+                return Users;
+            }
+
+            public User[] GetAllUsers(string name)
+            {
+                User[] Users;
+
+                XmlNodeList XmlUsers = this.GetElementsByTagName("User");
+                int Counter = 0;
+                int UserCounter = 0;
+                foreach (XmlNode XmlUser in XmlUsers)
+                {
+                    if (XmlUser.FirstChild.InnerText == name)
+                    {
+                        UserCounter++;
+                    }
+                    Counter++;
+                }
+
+                Users = new User[UserCounter];
+                Counter = 0;
+                foreach (XmlNode XmlUser in XmlUsers)
+                {
+                    if (XmlUser.FirstChild.InnerText == name)
+                    {
+                        int.TryParse(XmlUser.Attributes.GetNamedItem("App").InnerText, out int UserApp);
+                        Users[Counter] = new User(XmlUser.FirstChild.InnerText, XmlUser.LastChild.InnerText, UserApp);
+                        Counter++;
+                    }
+                }
+
+                return Users;
+            }
+
+            public void CreateUser(string username, string password, int app)
+            {
+                XmlElement NewUser = this.CreateElement("User");
+                NewUser.SetAttribute("App", app.ToString());
+                XmlElement Name = this.CreateElement("Name");
+                Name.InnerText = username;
+                NewUser.AppendChild(Name);
+                XmlElement Password = this.CreateElement("Password");
+                Password.InnerText = password;
+                NewUser.AppendChild(Password);
+            }
+
+            public User GetFirstUser(string name)
+            {
+                foreach (XmlNode XmlUser in this.GetElementsByTagName("User"))
+                {
+                    if (XmlUser.FirstChild.InnerText == name)
+                    {
+                        int.TryParse(XmlUser.Attributes.GetNamedItem("App").InnerText, out int UserApp);
+                        return new User(XmlUser.FirstChild.InnerText, XmlUser.LastChild.InnerText, UserApp);
+                    }
+                }
+                throw new UnknownUserException(String.Format("A User with name {0} could not be found.", name));
+            }
+
+            public User GetFirstUser(int app)
+            {
+                foreach (XmlNode XmlUser in this.GetElementsByTagName("User"))
+                {
+                    if (XmlUser.Attributes.GetNamedItem("App").InnerText == app.ToString())
+                    {
+                        int.TryParse(XmlUser.Attributes.GetNamedItem("App").InnerText, out int UserApp);
+                        return new User(XmlUser.FirstChild.InnerText, XmlUser.LastChild.InnerText, UserApp);
+                    }
+                }
+                throw new UnknownUserException(String.Format("A User with app {0} could not be found.", app.ToString()));
+            }
+        }
+
+        public class User
+        {
+            string Name { get; set; }
+            string Password { get; set; }
+            int App { get; set; }
+
+            public User()
+            {
+
+            }
+
+            public User(string name, string password, int app)
+            {
+                Name = name;
+                Password = password;
+                App = app;
+            }
+        }
+
+        [Serializable()]
+        public class UnknownUserException : System.Exception
+        {
+            public UnknownUserException() : base() { }
+            public UnknownUserException(string message) : base(message) { }
+            public UnknownUserException(string message, System.Exception inner) : base(message, inner) { }
+
+            protected UnknownUserException(System.Runtime.Serialization.SerializationInfo info,
+                System.Runtime.Serialization.StreamingContext context)
+            { }
         }
     }
 }
